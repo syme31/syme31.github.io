6ffb7166312dc3688ecb6f2bf3431d6a94c62a3e
Your Name <you@example.com>
2023-07-01 22:02:18 +0200

    share the default model (with the ruleId)

--- fca/main.js
+++ fca/main.js
@@ -40,6 +40,21 @@ function getRuleIdFromUrlParams() {
 	return ruleId;
 }
 
+function getDefaultModelFromUrlParams() {
+	const queryString = window.location.search;
+	const urlParams = new URLSearchParams(queryString);
+	if (!urlParams.has("dm")) {
+		return undefined;
+	}
+
+	const encoded = urlParams.get("dm");
+	if (!/^[0-9a-zA-Z/+]+(,[01]+)?$/.test(encoded)) {
+		return undefined;
+	}
+
+	return decodeModel(encoded);
+}
+
 function getValue(model, x, y) {
 	const size = model.length;
 	if (x < 0) {
@@ -244,21 +259,78 @@ function transpose(matrix) {
 	return firstRow.map((unusedValue, columnIndex) => matrix.map(row => row[columnIndex]));
 }
 
+const base64mapping = {
+	"000000": "A", "000001": "B", "000010": "C", "000011": "D",
+	"000100": "E", "000101": "F", "000110": "G", "000111": "H",
+	"001000": "I", "001001": "J", "001010": "K", "001011": "L",
+	"001100": "M", "001101": "N", "001110": "O", "001111": "P",
+	"010000": "Q", "010001": "R", "010010": "S", "010011": "T",
+	"010100": "U", "010101": "V", "010110": "W", "010111": "X",
+	"011000": "Y", "011001": "Z", "011010": "a", "011011": "b",
+	"011100": "c", "011101": "d", "011110": "e", "011111": "f",
+	"100000": "g", "100001": "h", "100010": "i", "100011": "j",
+	"100100": "k", "100101": "l", "100110": "m", "100111": "n",
+	"101000": "o", "101001": "p", "101010": "q", "101011": "r",
+	"101100": "s", "101101": "t", "101110": "u", "101111": "v",
+	"110000": "w", "110001": "x", "110010": "y", "110011": "z",
+	"110100": "0", "110101": "1", "110110": "2", "110111": "3",
+	"111000": "4", "111001": "5", "111010": "6", "111011": "7",
+	"111100": "8", "111101": "9", "111110": "+", "111111": "/",
+}
+
+function reverseMapping(o) {
+	return Object.keys(o)
+		.reduce((r, k) => Object.assign(r, {
+			[o[k]]: (r[o[k]] || []).concat(k)
+		}), {});
+}
+
+function encodeModel(model) {
+	const binaryString = model.map(row => row.join(""))
+		.join("");
+	const encoded = binaryString.match(/.{1,6}/g)
+		.map(s => base64mapping[s] || ("," + s))
+		.join("");
+	return encoded;
+}
+
+function decodeModel(encoded) {
+	const [b64, end] = encoded.split(",");
+
+	const base64reverseMapping = reverseMapping(base64mapping);
+	const binaryString = (b64 === ""
+		? ""
+		: (b64.match(/./g)
+			.map(c => (base64reverseMapping[c] || "x"))
+			.join("")))
+		+ (end || "");
+
+	const size = Math.sqrt(binaryString.length);
+	if (size % 1 !== 0) {
+		return undefined;
+	}
+
+	return binaryString.match(new RegExp("[01]{" + size + "}", "g"))
+		.map(s => s.match(/[01]/g));
+}
 
 let ruleId = getRuleIdFromUrlParams();
 if (ruleId === undefined) {
-	ruleId = 1385;
+	ruleId = 1385; // and 64133 are my favorites
 }
 const ruleIdTextField = document.getElementById("ruleId");
 ruleIdTextField.value = ruleId;
 ruleIdTextField.min = 0;
 ruleIdTextField.max = Math.pow(2, Math.pow(2, 4)) - 1;
 
-let defaultModel = transpose([
-	[0, 0, 0],
-	[0, 1, 0],
-	[0, 0, 0],
-]);
+let defaultModel = getDefaultModelFromUrlParams();
+if (defaultModel === undefined) {
+	defaultModel = transpose([
+		[0, 0, 0],
+		[0, 1, 0],
+		[0, 0, 0],
+	]);
+}
 
 const defaultModelTextarea = document.getElementById("default-model");
 defaultModelTextarea.value = transpose(defaultModel).map(row => row.join("")).join("\n");
@@ -292,10 +364,6 @@ const clickToZoom = event => {
 };
 mainCanvas.addEventListener("click", clickToZoom);
 
-document.getElementById("share-ruleId").addEventListener("click", event => {
-	prompt("URL", window.location.href.replace(/[?].*/, "") + "?ruleId=" + ruleId);
-	event.stopPropagation();
-});
 document.getElementById("td-for-ruleId").addEventListener("click", event => {
 	ruleIdTextField.focus();
 });
@@ -380,6 +448,13 @@ document.getElementById("td-for-reset").addEventListener("click", event => {
 document.getElementById("td-for-edit").addEventListener("click", event => {
 	layers.style.display = "none";
 	defaultModelTextarea.style.display = "block";
+});
+
+document.getElementById("td-for-share").addEventListener("click", event => {
+	const url = window.location.href.replace(/[?].*/, "")
+		+ "?" + "ruleId=" + ruleId
+		+ "&" + "dm=" + encodeModel(defaultModel);
+	prompt("URL", url);
 });
 
 updateDisplayScaleFactorAndCanvasSize();
